48dc0fe828580d5715beff66302cdbf3c8a44b99
Ryan Lei <ryanlei309@gmail.com>
2022-04-13 15:47:32 +0100

    t

--- script/plot3.js
+++ script/plot3.js
@@ -1,5 +1,5 @@
-let df3 = d3.csv("datasets/plot3.csv", function(data){
-    console.log(d3.csvParse(data));
-});
+// let df3 = d3.csv("datasets/plot3.csv", function(data){
+//     console.log(d3.csvParse(data));
+// });
 
-let dataset3 = d3.csvParse(df3)
+// let dataset3 = d3.csvParse(df3)
