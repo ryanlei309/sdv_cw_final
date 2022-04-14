b39706ca7e90e823a22ff6238960df1634f61cd6
Ryan Lei <ryanlei309@gmail.com>
2022-04-14 14:23:07 +0100

    t

--- scripts/plot3.js
+++ scripts/plot3.js
@@ -1,15 +1,27 @@
-import { select, csv } from "d3";
+// import { select, csv } from 'd3';
+const w = 920, h = 450
+const svg = d3.select("svg#bar-chart");
 
-const svg = select("svg#bar-chart");
+const width = +svg.attr('width', w);
+const height = +svg.attr('height', h);
 
-const width = +svg.attr('width');
-const height = +svg.attr('height');
+const render = data => {
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.duration_days)]);
 
-csv('plot3.csv').then(data => {
-    console.log(data);
+    console.log(xScale.domain());
+
+    svg.selectAll('rect').data(data)
+      .enter().append('rect')
+        .attr('width', 300)
+        .attr('height', 300)
+};
+
+d3.csv('datasets/plot3.csv').then(data => {
+    data.forEach(d => {
+        d.duration_days = +d.duration_days;
+    });
+    render(data);
 });
 
 
-
-    //現在這個 promise就是不知道什麼時候執行，架構就是你先把promise裡面的東西建構好
-    // 把data 傳到variable以後，
