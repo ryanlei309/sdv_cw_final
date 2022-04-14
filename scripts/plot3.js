4c41edae9c2ca4db9351ad56e795cb28a348c420
Ryan Lei <ryanlei309@gmail.com>
2022-04-14 14:40:29 +0100

    Update plot3.js

--- scripts/plot3.js
+++ scripts/plot3.js
@@ -1,4 +1,5 @@
-import { select, csv } from 'd3';
+// Reference: https://www.youtube.com/watch?v=NlBt-7PuaLk&list=WL&index=11&t=15s
+import { select, csv, scaleLinear, max, scaleBand} from 'd3';
 const w = 920, h = 450
 const svg = select("svg#bar-chart");
 
@@ -7,14 +8,20 @@ const height = +svg.attr('height', h);
 
 const render = data => {
     const xScale = scaleLinear()
-      .domain([0, max(data, d => d.duration_days)]);
+      .domain([0, max(data, d => d.duration_days)])
+      .range([0, width]);
 
-    console.log(xScale.domain());
+    const yScale = scaleBand()
+      .domain(data.map(d => d.clade))
+      .range([0, height]);
+
+    console.log(xScale.range());
 
     svg.selectAll('rect').data(data)
       .enter().append('rect')
-        .attr('width', 300)
-        .attr('height', 300)
+        .attr('y', d => yScale(d.clade))
+        .attr('width', d => xScale(d.duration_days))
+        .attr('height', yScale.bandwidth())
 };
 
 csv('datasets/plot3.csv').then(data => {
