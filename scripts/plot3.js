7c98dd79cc7bf8f2abb7d4c5bf03f205cf83fee4
Ryan Lei <ryanlei309@gmail.com>
2022-04-14 14:30:05 +0100

    Update plot3.js

--- scripts/plot3.js
+++ scripts/plot3.js
@@ -1,13 +1,13 @@
-// import { select, csv } from 'd3';
+import { select, csv } from 'd3';
 const w = 920, h = 450
-const svg = d3.select("svg#bar-chart");
+const svg = select("svg#bar-chart");
 
 const width = +svg.attr('width', w);
 const height = +svg.attr('height', h);
 
 const render = data => {
-    const xScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.duration_days)]);
+    const xScale = scaleLinear()
+      .domain([0, max(data, d => d.duration_days)]);
 
     console.log(xScale.domain());
 
@@ -17,7 +17,7 @@ const render = data => {
         .attr('height', 300)
 };
 
-d3.csv('datasets/plot3.csv').then(data => {
+csv('datasets/plot3.csv').then(data => {
     data.forEach(d => {
         d.duration_days = +d.duration_days;
     });
