21b5af5163a5519b782802d826ed17266f0dfe5b
Ryan Lei <ryanlei309@gmail.com>
2022-04-14 13:49:46 +0100

    test

--- scripts/plot3.js
+++ scripts/plot3.js
@@ -1,7 +1,15 @@
-let data3 = d3.csv("datasets/plot3.csv")
+import { select, csv } from "d3";
 
-let raw_data3
+const svg = select("svg#bar-chart");
 
-let data3 = data3.then(data => {
-    
-})
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+
+csv('plot3.csv').then(data => {
+    console.log(data);
+});
+
+
+
+    //現在這個 promise就是不知道什麼時候執行，架構就是你先把promise裡面的東西建構好
+    // 把data 傳到variable以後，
