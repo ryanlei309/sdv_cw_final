8e1ed3203337e473e715a1f9f3c17e5c7a3e6b8c
Ryan Lei <ryanlei309@gmail.com>
2022-04-14 19:00:20 +0100

    t

---
+++ scripts/plot2.js
@@ -0,0 +1,126 @@
+// Reference: https://vizhub.com/curran/2546209d161e4294802c4ac0098bebc2?edit=files&file=index.js
+// https://www.youtube.com/watch?v=0vKYFsTLtbA&t=901s
+// https://www.youtube.com/watch?v=xFI-us1moj0
+import {
+    select,
+    csv,
+    scaleLinear,
+    scaleTime,
+    scaleOrdinal,
+    extent,
+    axisLeft,
+    axisBottom,
+    line,
+    curveBasis,
+    nest,
+    schemeCategory10,
+    descending
+  } from 'd3';
+  
+import { colorLegend } from './scripts/colorLegend';
+const w = 920, h = 450
+const svg = select("svg#line-chart");
+
+const width = +svg.attr('width', w);
+const height = +svg.attr('height', h);
+
+const render = data => {
+    const xValue = d => d.days;
+    const xAxisLabel = 'Days';
+
+    const yValue = d => d.accumulate_region;
+    const yAxisLabel = 'Accumulated regions'
+
+    const colorValue = d => d.clade;
+
+    const margin = { top: 60, right: 220, bottom: 88, left: 105};
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top -  margin.bottom;
+
+    const xScale = scaleLinear()
+      .domain(extent(data, xValue))
+      .range([0, innerWidth])
+      .nice();
+
+    const yScale = scaleLinear()
+      .domain(extent(data, yValue))
+      .range([innerHeight, 0])
+      .nice();
+
+    const colorScale = scaleOrdinal(schemeCategory10);
+    
+    const g = svg.append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    
+    const xAxis = axisBottom(xScale)
+      .tickSize(-innerHeight)
+      .tickPadding(15);
+    
+    const yAxis = axisLeft(yScale)
+      .tickSize(-innerWidth)
+      .tickPadding(10);
+
+    const yAxisG = g.append('g').call(yAxis);
+    yAxisG.selectAll('.domain').remove();
+
+    yAxisG.append('text')
+      .attr('class', 'axis-label')
+      .attr('y', -65)
+      .attr('x', -innerHeight / 2)
+      .attr('fill', 'black')
+      .attr('transform', `rotate(-90)`)
+      .attr('text-anchor', 'middle')
+      .text(yAxisLabel);
+
+    const xAxisG = g.append('g').call(xAxis)
+      .attr('transform', `translate(0, ${innerHeight})`);
+
+    xAxisG.select('.domain').remove();
+
+    xAxisG.append('text')
+      .attr('class', 'axis-label')
+      .attr('y', 80)
+      .attr('x', innerWidth / 2)
+      .attr('fill', 'black')
+      .text(xAxisLabel);
+
+    const lineGenerator = line()
+      .x(d => xScale(xValue(d)))
+      .y(d => yScale(yValue(d)))
+      .curve(curveBasis);
+
+    const nested = nest()
+      .key(d => d.clade)
+      .entries(data);
+
+    colorScale.domain(nested.map(d => d.key));
+
+    g.selectAll('.line-path').data(nested)
+     .enter().append('path')
+      .attr('class', 'line-path')
+      .attr('d', d => lineGenerator(d.values))
+      .attr('stroke', d => colorScale(d.key));
+
+    g.append('text')
+      .attr('class', 'title')
+      .attr('y', -10)
+      .text(title);
+
+    svg.append('g')
+      .attr('transform', `translate(790,121)`)
+      .call(colorLegend, {
+        colorScale,
+        circleRadius: 13,
+        spacing: 30,
+        textOffset: 15
+      });
+};
+
+
+csv('datasets/plot2.csv').then(data => {
+    data.forEach(d => {
+        d.accumulate_region = +d.accumulate_region;
+        d.days = +d.days;
+    });
+    render(data);
+});
