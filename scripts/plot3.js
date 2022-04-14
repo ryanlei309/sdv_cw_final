0c539cea55bf3e470b2803f9e3e9c16e1d382965
Ryan Lei <ryanlei309@gmail.com>
2022-04-14 15:44:43 +0100

    Update plot3.js

--- scripts/plot3.js
+++ scripts/plot3.js
@@ -1,10 +1,10 @@
 // Reference: https://www.youtube.com/watch?v=NlBt-7PuaLk&list=WL&index=11&t=15s
-import { select, csv, scaleLinear, max, scaleBand, axisLeft, axisBottom, format} from 'd3';
+// import { select, csv, scaleLinear, max, scaleBand, axisLeft, axisBottom, format} from 'd3';
 const w = 920, h = 450
 const xAxisLabelText = 'Days';
 
 
-const svg = select("svg#bar-chart");
+const svg = d3.select("svg#bar-chart");
 
 const width = +svg.attr('width', w);
 const height = +svg.attr('height', h);
@@ -16,11 +16,11 @@ const render = data => {
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
     
-    const xScale = scaleLinear()
-      .domain([0, max(data, xValue)])
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(data, xValue)])
       .range([0, innerWidth]);
 
-    const yScale = scaleBand()
+    const yScale = d3.scaleBand()
       .domain(data.map(yValue))
       .range([0, innerHeight])
       .padding(0.1);
@@ -28,9 +28,9 @@ const render = data => {
     const g = svg.append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    g.append('g').call(axisLeft(yScale));
-    g.append('g').call(axisBottom(xScale))
-      .attr('transform', 'translate(0, ${innerHeight})');
+    g.append('g').call(d3.axisLeft(yScale));
+    g.append('g').call(d3.axisBottom(xScale))
+      .attr('transform', `translate(0, ${innerHeight})`);
 
     g.selectAll('rect').data(data)
       .enter().append('rect')
@@ -39,7 +39,7 @@ const render = data => {
         .attr('height', yScale.bandwidth());
 };
 
-csv('datasets/plot3.csv').then(data => {
+d3.csv('datasets/plot3.csv').then(data => {
     data.forEach(d => {
         d.duration_days = +d.duration_days;
     });
