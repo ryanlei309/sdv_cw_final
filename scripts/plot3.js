f120c4ba3b148c04accb282884b1e4a16614b6f1
Ryan Lei <ryanlei309@gmail.com>
2022-04-14 19:11:43 +0100

    p

--- scripts/plot3.js
+++ scripts/plot3.js
@@ -1,4 +1,5 @@
 // Reference: https://www.youtube.com/watch?v=NlBt-7PuaLk&list=WL&index=11&t=15s
+// Reference: https://vizhub.com/curran/326730c80fc54969ae501fb58c8cb94b
 // import { select, csv, scaleLinear, max, scaleBand, axisLeft, axisBottom, format} from 'd3';
 const w = 920, h = 450
 const xAxisLabelText = 'Days';
