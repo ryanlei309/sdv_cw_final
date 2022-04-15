9e3346f0686f395acb61072b6f2a0b24246e4c8b
Ryan Lei <ryanlei309@gmail.com>
2022-04-15 15:15:43 +0100

    Update plot2.js

--- scripts/plot2.js
+++ scripts/plot2.js
@@ -17,37 +17,37 @@ const {
   
 // import { colorLegend } from './colorLegend';
 
-const colorLegend = (selection, props) => {
-    const {
-        colorScale,
-        circleRadius,
-        spacing,
-        textOffset
-    } = props;
+// const colorLegend = (selection, props) => {
+//     const {
+//         colorScale,
+//         circleRadius,
+//         spacing,
+//         textOffset
+//     } = props;
 
-    const groups = selection.selectAll('g')
-        .data(colorScale.domain());
-    const groupsEnter = groups
-        .enter().append('g')
-        .attr('class', 'tick');
-    groupsEnter
-        .merge(groups)
-        .attr('transform', (d, i) =>
-            `translate(0, ${i * spacing})`
-        );
-    groups.exit().remove();
+//     const groups = selection.selectAll('g')
+//         .data(colorScale.domain());
+//     const groupsEnter = groups
+//         .enter().append('g')
+//         .attr('class', 'tick');
+//     groupsEnter
+//         .merge(groups)
+//         .attr('transform', (d, i) =>
+//             `translate(0, ${i * spacing})`
+//         );
+//     groups.exit().remove();
 
-    groupsEnter.append('circle')
-        .merge(groups.select('circle'))
-        .attr('r', circleRadius)
-        .attr('fill', colorScale);
+//     groupsEnter.append('circle')
+//         .merge(groups.select('circle'))
+//         .attr('r', circleRadius)
+//         .attr('fill', colorScale);
 
-    groupsEnter.append('text')
-        .merge(groups.select('text'))
-        .text(d => d)
-        .attr('dy', '0.32em')
-        .attr('x', textOffset)
-};
+//     groupsEnter.append('text')
+//         .merge(groups.select('text'))
+//         .text(d => d)
+//         .attr('dy', '0.32em')
+//         .attr('x', textOffset)
+// };
 
 const width = 600, height = 450
 
