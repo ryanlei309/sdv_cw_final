606d010f6db52fa8ab553f959ae73619a5468802
Ryan Lei <ryanlei309@gmail.com>
2022-04-17 18:15:49 +0100

    Update plot3.js

--- scripts/plot3.js
+++ scripts/plot3.js
@@ -1,6 +1,6 @@
 // Reference: https://www.youtube.com/watch?v=NlBt-7PuaLk&list=WL&index=11&t=15s
 // Reference: https://vizhub.com/curran/326730c80fc54969ae501fb58c8cb94b
-const { select, csv, scaleLinear, max, scaleBand, axisLeft, axisBottom, format, scaleOrdinal, schemeSpectral, color} = d3
+const { select, csv, scaleLinear, max, scaleBand, axisLeft, axisBottom, format, scaleOrdinal, interpolateSpectral, schemeSpectral, color} = d3
 const width = 600, height = 450
 const xAxisLabelText = 'Days';
 
@@ -31,7 +31,7 @@ const render = data => {
       .range([0, innerHeight])
       .padding(0.1);
 
-    const colorScale = scaleOrdinal(schemeSpectral[21]);
+    const colorScale = interpolateSpectral(schemeSpectral[21]);
 
     const g = svg.append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
